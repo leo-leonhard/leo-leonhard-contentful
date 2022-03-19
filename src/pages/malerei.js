d58d8312b5e91cdd751b02015d9358426fddf8c0
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-19 17:58:30 -0400

    Styling.

--- src/pages/malerei.js
+++ src/pages/malerei.js
@@ -26,13 +26,15 @@ const MalereiPage = ({ data }) => {
             <h2>DAS ZEICHNERISCHE WERK</h2>
             <hr />
             <div className="d-flex justify-content-between">
-            {data.artwork.nodes.map(artwork => (
-                <div key={`artwork-${artwork.slug}`} style={{width: "23%"}}>
-                    <h3>{artwork.title},{artwork.year}</h3>
-                    <h4>{artwork.type}</h4>
-                    <img style={{width: "100%"}} src={artwork.image.url}/>
+                {data.artwork.nodes.map(artwork => (
+                    <div key={`artwork-${artwork.slug}`} className="d-flex flex-column justify-content-between" style={{width: "23%"}}>
+                        <div className="mb-3">
+                            <h3>{artwork.title},{artwork.year}</h3>
+                            <h4>{artwork.type}</h4>
+                        </div>
+                        <img style={{width: "100%"}} src={artwork.image.url}/>
                     </div>
-            ))}
+                ))}
             </div>
         </Layout>
     )
