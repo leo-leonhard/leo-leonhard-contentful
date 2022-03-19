b0ceaff086007d186d402422b8cda7ea1eca60ba
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-19 18:05:23 -0400

    Mobile query.

--- src/pages/malerei.js
+++ src/pages/malerei.js
@@ -25,9 +25,9 @@ const MalereiPage = ({ data }) => {
         <Layout>
             <h2>DAS ZEICHNERISCHE WERK</h2>
             <hr />
-            <div className="d-flex justify-content-between">
+            <div className="d-flex justify-content-between werke-container">
                 {data.artwork.nodes.map(artwork => (
-                    <div key={`artwork-${artwork.slug}`} className="d-flex flex-column justify-content-between" style={{width: "23%"}}>
+                    <div key={`artwork-${artwork.slug}`} className="d-flex flex-column justify-content-between werk" style={{width: "23%"}}>
                         <div className="mb-3">
                             <h3>{artwork.title},{artwork.year}</h3>
                             <h4>{artwork.type}</h4>
