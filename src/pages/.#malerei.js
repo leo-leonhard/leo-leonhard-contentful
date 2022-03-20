f0a764ad117824904ea8f1d5adc5305d42075693
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-19 20:46:02 -0400

    Had to hardcode images because Contentful does not allow to reorder them since years of ignoring their customers.

--- src/pages/.#malerei.js
+++
@@ -1,1 +0,0 @@
-hzr@badlands.5439
--- src/pages/malerei.js
+++ src/pages/malerei.js
@@ -2,23 +2,41 @@ import * as React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 
+import Hippo from '../images/Hippo.jpg'
+import Keiler from '../images/Keiler.jpg'
+import Szene from '../images/Szene.jpg'
+import Blues from '../images/Blues.jpg'
 
-export const query = graphql`
-{
-  artwork: allContentfulArtwork {
-    nodes {
-      title
-      year
-      type
-      slug
-      image {
-        url
-      }
-    }
-  }
-}
-`
-
+const artworks = [
+    {
+        title: "Hippo",
+        year: "1984",
+        type: "Bleistift auf Papier",
+        image: Hippo,
+        slug: "hippo"
+    },
+    {
+        title: "Keiler",
+        year: "1984",
+        type: "Bleistift auf Papier",
+        image: Keiler,
+        slug: "keiler"
+    },
+    {
+        title: "Häusliche Szene",
+        year: "1984",
+        type: "Radierung",
+        image: Szene,
+        slug: "haeusliche_szene"
+    },
+    {
+        title: "BLUES BROTHERS",
+        year: "1985",
+        type: "Radierung",
+        image: Blues,
+        slug: "blues_brothers"
+    },
+]
 
 const MalereiPage = ({ data }) => {
     return (
@@ -26,13 +44,17 @@ const MalereiPage = ({ data }) => {
             <h2>DAS ZEICHNERISCHE WERK</h2>
             <hr />
             <div className="d-flex justify-content-between werke-container flex-wrap">
-                {data.artwork.nodes.map(artwork => (
-                    <div key={`artwork-${artwork.slug}`} className="mt-3 d-flex flex-column justify-content-between werk" style={{width: "23%"}}>
+                {artworks.map(artwork => (
+                    <div
+                        key={artwork.slug}
+                        className="mt-3 d-flex flex-column justify-content-between werk"
+                        style={{width: "23%"
+                               }}>
                         <div className="mb-3">
                             <h3>{artwork.title},{artwork.year}</h3>
                             <h4>{artwork.type}</h4>
                         </div>
-                        <img style={{width: "100%"}} src={artwork.image.url}/>
+                        <img style={{width: "100%"}} src={artwork.image}/>
                     </div>
                 ))}
             </div>
