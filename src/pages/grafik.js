63b1999a626f38b672aa0b8392c05f817c9025f5
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-03-22 19:47:29 -0400

    Responsive styling.

--- src/pages/grafik.js
+++ src/pages/grafik.js
@@ -45,22 +45,22 @@ function GrafikPage({ data }) {
             <hr />
             <div className="d-flex justify-content-between werke-container flex-wrap">
                 {horizontalImages.map(artwork => (
-                    <div key={`artwork-${artwork.slug}`} className="mt-1 mb-1 d-flex flex-column justify-content-between werk-horizontal" style={{width: "23%"}}>
+                    <div key={`artwork-${artwork.slug}`} className="mt-1 mb-1 d-flex flex-column justify-content-between werk werk-horizontal" style={{width: "23%"}}>
                         <div className="mb-3">
                             <h3>{artwork.title},{artwork.year}</h3>
                             <h4>{artwork.type}</h4>
                         </div>
-                        <Link to={artwork.slug}><img style={{width: "100%"}} src={artwork.image.url} alt={artwork.title}/></Link>
+                        <Link className="artwork-overview" to={artwork.slug}><img src={artwork.image.url} alt={artwork.title}/></Link>
                     </div>
                 ))}
                 <hr/>
                 {verticalImages.map(artwork => (
-                    <div key={`artwork-${artwork.slug}`} className="mt-3 d-flex flex-column justify-content-between werk-vertical" style={{width: "23%"}}>
+                    <div key={`artwork-${artwork.slug}`} className="mt-3 d-flex flex-column justify-content-between werk werk-vertical" style={{width: "23%"}}>
                         <div className="mb-3">
                             <h3>{artwork.title},{artwork.year}</h3>
                             <h4>{artwork.type}</h4>
                         </div>
-                        <Link to={artwork.slug}><img style={{width: "100%"}} src={artwork.image.url} alt={artwork.title}/></Link>
+                        <Link className="artwork-overview" to={artwork.slug}><img src={artwork.image.url} alt={artwork.title}/></Link>
                     </div>
                 ))}
             </div>
