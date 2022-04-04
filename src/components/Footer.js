d056b6424c3dbefc8384644f19ab0ce67bf3b1eb
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-04-04 12:41:38 -0400

    Navigation and pages.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import logo from '../images/logo_white.png'
 
+import '../styles/footer-style.css'
+
 const Footer = () => (
-    <div className="footer" style={{background: "#589AAD", width:"90%", margin: "0 auto"}}>
-        <div style={{padding: "1em 2em"}}>
-            <img width="15%" src={logo}/>
-            <div className="address" style={{color: "white", margin: "1em auto"}}>
+    <div className="footer">
+        <div>
+            <Link to="/"><img src={logo}/></Link>
+            <div className="address" style={{margin: "1em auto"}}>
                 <p>LEO LEONHARD NACHLASS</p>
                 <p>c/o Florian Leonhard</p>
                 <p>Strasse 12</p>
@@ -13,8 +16,37 @@ const Footer = () => (
                 <p>info@leoleonhard.de</p>
             </div>
         </div>
-        <div>
+        <div className="d-flex footer-nav">
+            <div className="footer-nav-item">
+                <hr/>
+                <Link to="/lebenslauf">LEBENSLAUF</Link>
+                <hr/>
+            </div>
+            <div className="footer-nav-item">
+                <hr/>
+                <Link to="/malerei">MALEREI</Link>
+                <hr/>
+            </div>
+            <div className="footer-nav-item">
+                <hr/>
+                <Link to="/grafik">GRAFIK</Link>
+                <hr/>
+            </div>
+            <div className="footer-nav-item">
+                <hr/>
+                <Link to="/publikationen">PUBLIKATIONEN</Link>
+                <hr/>
+            </div>
+            <div className="footer-nav-item">
+                <hr/>
+                <Link to="">IMPRESSUM</Link>
+                <Link to="">DATENSCHUTZ</Link>
+                <Link to="">PRESSE</Link>
+                <Link to="">BILDNACHWEIS</Link>
+                <hr/>
+            </div>
         </div>
+
     </div>
 )
 
