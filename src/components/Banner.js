dc9b74c2d5f5282cd9dadb6b734d35f28e810d3e
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-04-12 14:57:03 -0400

    PageBanner MalereiPage.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 
 
-const Banner = () => {
-
+const Banner = ({ data }) => {
     return (
+        <>
         <StaticQuery
             query={graphql`
               query BannerQuery {
@@ -12,19 +12,34 @@ const Banner = () => {
                     nodes {
                         slug
                         image {
-                            gatsbyImage(width: 1200)
-                            url
+                          id
+                          resize(width: 1200) {
+                            width
+                            height
+                            src
+                          }
                         }
                     }
                  }
               }
           `}
-            render={data => {
-                {data.allContentfulBannerImage.nodes.map((image) => {
-                    console.log(image)
-                })}
-            }}
-       />
+            render={data => (
+                <div>
+                    {data.allContentfulBannerImage.nodes.map((image) => {
+                        return (
+                            <div>
+                                <img className="mb-5" src={image.image.resize.src} style={{float: "left", width: "100%", height: "90vh", objectFit: "cover"}}/>
+                            </div>
+                        )
+                    })}
+
+                </div>
+            )}
+        />
+            <div>
+                <p className="p-3 mt-5" style={{fontFamily: "EB Garamond, serif", fontSize: "1.3em"}}>Aliquam erat volutpat.  Nunc eleifend leo vitae magna.  In id erat non orci commodo lobortis.  Proin neque massa, cursus ut, gravida ut, lobortis eget, lacus.  Sed diam.  Praesent fermentum tempor tellus.  Nullam tempus.  Mauris ac felis vel velit tristique imperdiet.  Donec at pede.  Etiam vel neque nec dui dignissim bibendum.  Vivamus id enim.  Phasellus neque orci, porta a, aliquet quis, semper a, massa.  Phasellus purus.  Pellentesque tristique imperdiet tortor.  Nam euismod tellus id erat.</p>
+            </div>
+            </>
     )
 }
 
