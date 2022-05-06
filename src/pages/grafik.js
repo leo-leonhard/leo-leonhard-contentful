646bdedae263456708c387dc73f7e37b7a031e81
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-05-06 10:13:30 -0400

    Style.

--- src/pages/grafik.js
+++ src/pages/grafik.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import {graphql} from 'gatsby'
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import Layout from '../components/Layout'
 import Banner from '../components/Banner'
 
 
-const GrafikPage = ( { data } ) => (
+const GrafikPage = ( { data } ) => {
+    const grafiken = data.allContentfulGrafik.nodes
+
+    return(
     <Layout>
       <Banner image={data.allContentfulBannerImage.nodes}/>
+      <div className="d-flex flex-wrap" style={{width: "100%"}}>
+          {grafiken.map(grafik => {
+              const image = getImage(grafik.image)
+              return(
+                  <div>
+                    <GatsbyImage image={image} alt={grafik.title}/>
+                  </div>
+              )
+        })}
+        </div>
     </Layout>
-)
+    )
+}
 
 export const query = graphql`
 query getGrafikContent {
@@ -23,6 +38,16 @@ query getGrafikContent {
           height
           src
         }
+      }
+    }
+  }
+ allContentfulGrafik {
+    nodes {
+      id
+      year
+      title
+      image {
+        gatsbyImageData(width: 300, placeholder: DOMINANT_COLOR)
       }
     }
   }
