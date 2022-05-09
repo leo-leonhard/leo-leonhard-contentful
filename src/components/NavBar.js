f85438f1e8bb3a13d47b060be0a0c964bc667d02
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-05-09 12:12:45 -0400

    Navbarstyling.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,8 +17,8 @@ const NavBar = class extends React.Component {
                         <img className="logo" style={{margin: "0 0.3em"}} src={logo}/>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <Navbar.Collapse style={{width: "42%"}}>
-                        <Nav className="me-auto">
+            <Navbar.Collapse style={{width: "40%"}}>
+              <Nav className="me-auto">
                             <Nav.Link href="/lebenslauf">LEBENSLAUF</Nav.Link>
                             <Nav.Link href="/malerei">MALEREI</Nav.Link>
                             <Nav.Link href="/grafik">GRAFIK</Nav.Link>
