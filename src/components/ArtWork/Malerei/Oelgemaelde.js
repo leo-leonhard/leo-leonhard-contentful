74c3f95069a9a09a20649b861b34e1e60cf1a289
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-05-12 15:43:06 -0400

    Cleanup.

--- src/components/ArtWork/Malerei/Oelgemaelde.js
+++ src/components/ArtWork/Malerei/Oelgemaelde.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 
-import KunstWerk from '../../KunstWerk'
+import KunstWerk from '../KunstWerk'
 
 
 const Oelgemaelde = ({ data }) => (
