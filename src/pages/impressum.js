df4c2f9f88d1757aa47359fcd5e97ae9b5c5ab9a
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-05-12 10:24:46 -0400

    Source Datenschutztext from Contentful.

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -1,68 +1,64 @@
 import React from "react"
+import { graphql } from 'gatsby'
+import { MDXProvider } from "@mdx-js/react"
+import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer"
 
 import Layout from "../components/Layout"
 
-const Impressum = () => {
 
+
+const Impressum = ({data}) => {
+    console.log(data)
     return(
         <Layout>
-        <div style={{lineHeight: "0.3", marginBottom: "3em"}}>
-          <h3>IMPRESSUM</h3>
-          <p>Angaben gemäß § 5 TMG</p>
-          <br/>
-          <br/>
-          <br/>
-          <p><b>Nachlaß Leo Leonhard</b></p>
-          <p>Florian Leonhard</p>
-          <p>Sandstraße 18</p>
-          <p>64404 Bickenbach</p>
-          <br/>
-          <br/>
-          <br/>
-          <p><b>Kontakt</b></p>
-          <p>E-Mail: info@leoleonhard.de</p>
-          <br/>
-          <br/>
-          <br/>
-          <p>Quelle:</p>
-          <p>e-recht24.de</p>
-        </div>
-            <div>
-              <hr/>
-              <h3>DATENSCHUTZERKLÄRUNG</h3>
-              <p>1. DATENSCHUTZ AUF EINEN BLICK</p>
-              <p>Allgemeine Hinweise</p>
-              <p>Die folgenden Hinweise geben einen einfachen Überblick daruNber, was mit Ihren personenbezogenen Daten passiert, wenn Sie diese Website besu-
-              chen. Personenbezogene Daten sind alle Daten, mit denen Sie persönlich identifiziert werden können. AusfuNhrliche Informationen zum Thema Da-
-              tenschutz entnehmen die unserer unter diesem Text aufgefüNhrten Datenschutzerklärung.</p>
-<br/>
-              <h4>DATENERFASSUNG AUF DIESER WEBSITE</h4>
-              <p>Wer ist verantwortlich fur die Datenerfassung auf dieser Website?</p>
-              <p>Die Datenverarbeitung auf dieser Website erfolgt durch den Websitebetreiber. Dessen Kontaktdaten können Sie dem Abschnitt „Hinweis zur Ver-
-              antwortlichen Stelle“ in dieser Datenschutzerklärung entnehmen.</p>
-              <p>Wie erfassen wir Ihre Daten?</p>
-              <p>Ihre Daten werden zum einen dadurch erhoben, dass Sie uns diese mitteilen. Hierbei kann es sich z. B. um Daten handeln, die Sie in ein Kontaktfor-
-              mular eingeben. Andere Daten werden automatisch oder nach Ihrer Einwilligung beim Besuch der Website durch unsere ITSysteme erfasst. Das sind vor allem technische Daten (z. B. Internetbrowser, Betriebssystem oder Uhrzeit des Seitenaufrufs). Die Erfassung dieser Daten erfolgt automatisch,
-                sobald Sie diese Website betreten.</p>
-              <br/>
-              <h4>Wofur nutzen wir Ihre Daten?</h4>
-              <p>Ein Teil der Daten wird erhoben, um eine fehlerfreie Bereitstellung der Website zu gewährleisten. Andere Daten können zur Analyse Ihres Nutzerverhaltens verwendet werden.</p>
-              <h4>Welche Rechte haben Sie bezüuglich Ihrer Daten?</h4>
-        <p>Sie haben jederzeit das Recht, unentgeltlich Auskunft über Herkunft, Empfänger und Zweck Ihrer gespeicherten personenbezogenen Daten zu erhalten. Sie haben außerdem ein Recht, die Berichtigung oder Löschung dieser Daten zu verlangen. Wenn Sie eine Einwilligung zur Datenverarbeitung erteilt haben, können Sie diese Einwilligung jederzeit fuNr die Zukunft widerrufen. Außerdem haben Sie das Recht, unter bestimmten Umständen die Einschränkung der Verarbeitung Ihrer personenbezogenen Daten zu verlangen. Des Weiteren steht Ihnen ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Hierzu sowie zu weiteren Fragen zum Thema Datenschutz können Sie sich jederzeit an uns wenden. Analyse-Tools und Tools von Drittanbietern Beim Besuch dieser Website kann Ihr Surf-Verhalten statistisch ausgewertet werden. Das geschieht vor allem mit sogenannten Analyseprogrammen. Detaillierte Informationen zu diesen Analyseprogrammen finden Sie in der folgenden Datenschutzerklärung.</p>
-              <h4>2. HOSTING</h4>
-              <h4>Externes Hosting</h4>
-              <p>Diese Website wird bei einem externen Dienstleister gehostet (Hoster). Die personenbezogenen Daten, die auf dieser Website erfasst werden, werden auf den Servern des Hosters gespeichert. Hierbei kann es sich v.a. um IP-Adressen, Kontaktanfragen, Meta- und Kommunikationsdaten, Vertragsdaten, Kontaktdaten, Namen, Websitezugriffe und sonstige Daten, die uNber eine Website generiert werden, handeln. Der Einsatz des Hosters erfolgt zum Zwecke der VertragserfüNllung gegenuNber unseren potenziellen und bestehenden Kunden (Art. 6 Abs. 1 lit. b DSGVO) und im Interesse einer sicheren, schnellen und effizienten Bereitstellung unseres Online-Angebots durch einen professionellen Anbieter (Art. 6 Abs. 1 lit. f DSGVO). Sofern eine entsprechende Einwilligung abgefragt wurde, erfolgt die Verarbeitung ausschließlich auf Grundlage von Art. 6 Abs. 1 lit. a DSGVO und §25 Abs. 1 TTDSG, soweit die Einwilligung die Speicherung von Cookies oder den Zugriff auf Informationen im Endgerät des Nutzers (z. B. Device-Fingerprinting) im Sinne des TTDSG umfasst. Die Einwilligung ist jederzeit widerrufbar. Unser Hoster wird Ihre Daten nur insoweit verarbeiten, wie dies zur ErfuNllung seiner Leistungspflichten erforderlich ist und unsere Weisungen in Bezug auf diese Daten befolgen.</p>
-              <br/>
-              <div style={{lineHeight: "0.3"}}>
-              <p>Wir setzen folgenden Hoster ein:</p>
-              <p>Musterhosting AG</p>
-              <p>Musterweg 100</p>
-              <p>90210 Musterstadt</p>
-        </div>
-            </div>
+          <div style={{lineHeight: "0.3", marginBottom: "3em"}}>
+            <h1>IMPRESSUM</h1>
+            <p>Angaben gemäß § 5 TMG</p>
+            <br/>
+            <br/>
+            <br/>
+            <p><b>Nachlaß Leo Leonhard</b></p>
+            <p>Florian Leonhard</p>
+            <p>Sandstraße 18</p>
+            <p>64404 Bickenbach</p>
+            <br/>
+            <br/>
+            <br/>
+            <p><b>Kontakt</b></p>
+            <p>E-Mail: info@leoleonhard.de</p>
+            <br/>
+            <br/>
+            <br/>
+            <p>Quelle:</p>
+            <p>e-recht24.de</p>
+          </div>
+          <div id="datenschutzerklaerung">
+            <hr/>
+            <h1>Datenschutzerklärung</h1>
+            <br/>
+            <MDXProvider>
+              <MDXRenderer>
+                {data.contentfulDatenschutzerklaerung.text.childMdx.body}
+              </MDXRenderer>
+            </MDXProvider>
+          </div>
           <br/>
         </Layout>
     )
 }
+
+
+export const query = graphql`
+query DatenschutzQuery {
+  contentfulDatenschutzerklaerung {
+      text {
+        childMdx {
+          body
+        }
+      }
+  }
+}
+`
+
 
 export default Impressum
