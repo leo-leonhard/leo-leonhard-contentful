ae84a818f1222b32ac18248a0e190555a094d12d
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-05-13 13:39:14 -0400

    Style ArtWork component, space between.

--- src/components/ArtWork/KunstWerk.js
+++ src/components/ArtWork/KunstWerk.js
@@ -6,45 +6,15 @@ import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer"
 import  { Modal, Button, useState } from "react-bootstrap"
 
 
-function Example() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
-}
-
 
 export default function Kunstwerk(props) {
     const kunstwerk = props.kunstwerk
     return(
-        <div className="d-flex flex-wrap justify-content-between">
+        <div className="d-flex flex-wrap justify-content-between" style={{maxWidth: "100%", gap: "1em"}}>
         {kunstwerk.map(werk => {
             const image = getImage(werk.image)
             return(
-                <div className="d-flex flex-column mb-5">
+                <div className="d-flex flex-column mb-5"  style={{width: "20%"}}>
                   <h3>{werk.title}, {werk.year}</h3>
                   <GatsbyImage image={image}/>
                 </div>
