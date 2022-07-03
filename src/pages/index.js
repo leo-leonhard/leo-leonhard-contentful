0555e8b7f67798a2e5db0ed28ab8e0e15fa73de3
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-07-03 16:49:25 -0400

    Style.

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,8 +39,9 @@ const IndexPage = ({ data }) => {
 
                 </div>
                 <div>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.  Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.  Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl.
-            </div>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.  Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.  Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl. <span style={{margin: "0 0.5em"}}><b>mehr...</b></span>
+                </div>
+
               </div>
             </div>
         </Layout>
