c6c12b31feacb39b20cb03486035bb46089a624e
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-07-03 12:13:31 -0400

    Page width.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,7 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`
     },
     plugins: [
-        {
-            resolve: 'gatsby-plugin-mailchimp',
-            options: {
-                endpoint: '7081bbc3f0', // string; add your MC list endpoint here; see instructions below
-                timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
-            },
-        },
+
         {
             resolve: 'gatsby-source-contentful',
             options: {
