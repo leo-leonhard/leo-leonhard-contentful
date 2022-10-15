49bdddf710171091192cf52e041c1b4d1485402c
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-10-15 11:55:05 -0400

    Abstaende.

--- src/components/ArtWork/KunstWerk.js
+++ src/components/ArtWork/KunstWerk.js
@@ -6,13 +6,16 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 export default function Kunstwerk(props) {
     const kunstwerk = props.kunstwerk
     return(
-        <div className="d-flex flex-wrap justify-content-between" style={{maxWidth: "100%", gap: "1em"}}>
+        <div
+          className="d-flex flex-wrap justify-content-between"
+          style={{maxWidth: "100%", gap: "1em"}}
+        >
           {kunstwerk.map(werk => {
               const image = getImage(werk.image)
               return(
                   <div className="d-flex flex-column mb-5"  style={{width: "20%"}}>
-                    <Link to={werk.slug} style={{textDecoration: "none", color: "black"}}>
-                      <h4 style={{height: "2em"}}>{werk.title}, {werk.year}</h4>
+                    <Link to={werk.slug} style={{textDecoration: "none", color: "black", display: "flex", flexDirection: "column"}}>
+                      <h4 style={{minHeight: "2em"}}>{werk.title}, {werk.year}</h4>
                       <GatsbyImage image={image}/>
                     </Link>
                   </div>
