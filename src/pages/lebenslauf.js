a30097ce39bbd595af8f6f1437655a6724172cd0
darayaq-01 <daniel@darayaq.de>
2023-02-13 12:44:56 +0100

    fix card sizes & logo

--- src/pages/lebenslauf.js
+++ src/pages/lebenslauf.js
@@ -4,10 +4,10 @@ import {graphql, Link} from 'gatsby'
 // import { MDXProvider } from "@mdx-js/react"
 // import MDXRenderer from "gatsby-plugin-mdx/mdx-renderer"
 
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
+
 import PageBanner from '../components/PageBanner'
 import IntroText from '../components/IntroText'
+import Layout from '../components/Layout'
 
 import portrait from "../images/portrait.jpg"
 // import selbstportraet from "../images/selbstporttraet.png"
@@ -17,54 +17,71 @@ import s from "../images/s.png"
 import p from "../images/p.png"
 
 
-const Box = (props) => {
-    const title = props.title
-    const image = props.image
-    const destination = props.destination
+const Box = ({ title, image, destination }) => {
     return (
         <div style={{width: "20%", margin: "1em"}}>
           <Link to={destination} style={{color: "#212529"}}>
             <img src={image} alt="text" style={{width: "100%",  height: "8em"}}/>
             <div className="mt-1">
 
-              <button className="fett" style={{width: "100%", fontSize: "1vw", background: "#589AAD", border: "3px solid #589AAD", color: "snow", textTransform: "uppercase"}}>{title}</button>
+              {/* 
+              <button className="fett" style={{width: "100%", fontSize: "1vw", background: "#589AAD", border: "3px solid #589AAD", color: "snow", textTransform: "uppercase"}}>{title}</button> 
+              */}
+              <button className="btn btn-stationen">{title}</button>
             </div>
         </Link>
         </div>
     )}
 
+    const lebenslaufStationen = [
+      {  
+        title: "Lebensstationen",
+        destination: "/lebensstationen",
+        image:` ${s}`
+      },
+      {
+        title: "Nachruf",
+        destination: "/test",
+        image: `${portrait}`
+      },
+      {
+        title: "Auszeichnungen",
+        destination: "/auszeichnungen",
+        image: `${p}`
+      },
+      {  
+        title: "Ausstellungen",
+        destination: "/ausstellungen",
+        image: `${auszeichnungen}`
+      } 
+    ]
+
 const LebenslaufPage = ({ data }) => (
-    <>
-      <NavBar/>
-      <div style={{width: "75%", margin: "1em auto"}}>
+    <Layout>
+      
         <section style={{background: "#F8F3EA", padding: "1em 2em 3em 2em"}}>
           <PageBanner image={data.allContentfulPageBanner.nodes}/>
           <IntroText
             introdata={data.allContentfulIntroText.nodes}
           />
         </section>
-        <br/>
-        <div className="d-flex justify-content-between mt-3 mb-5">
-          <Box
-            title="Lebensstationen"
-            destination="/lebensstationen"
-            image={s}/>
-          <Box
-            title="Nachruf"
-            destination="/test"
-            image={portrait}/>
-          <Box
-            title="Auszeichnungen"
-            destination="/auszeichnungen"
-            image={p}/>
-          <Box
-            title="Ausstellungen"
-            destination="/ausstellungen"
-            image={auszeichnungen}/>
+      
+        <div className="d-flex justify-content-between mt-5 mb-5">
+
+          {
+            lebenslaufStationen.map((item) => {
+              return (
+                <Box 
+                    key={item.title}
+                    title={item.title}
+                    destination={item.destination}
+                    image={item.image}
+                />
+              )}
+          )}
         </div>
-      </div>
-      <Footer/>
-    </>
+      
+      </Layout>
 )
 
 
