de77cd5ef62ecbd9b772e72b5bfcf8e63ba7ac2b
darayaq-01 <daniel@darayaq.de>
2023-03-11 05:38:05 +0100

    refactored showcase

--- src/components/showCaseBox/ShowCaseBoxPage.js
+++ src/components/showCaseBox/ShowCaseBoxPage.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { GatsbyImage } from 'gatsby-plugin-image'
+
+import { MDXProvider } from '@mdx-js/react'
+import MDXRenderer from 'gatsby-plugin-mdx/mdx-renderer'
+
 import './showCaseBox.css'
 
 export default function ShowCaseBoxPage({ alt, src, text }) {
     return (
-        <section className="showcase-page">
-            <img src={src} alt={alt} className="img-showcase" />
-            <div className="showcase-text-page">{text}</div>
+        <section className="standard-layout-width">
+            <div className="showcase-page">
+                <GatsbyImage alt={alt} className="img-showcase" image={src} />
+
+                {/* <img src={src} alt={alt} className="img-showcase" /> */}
+                <div className="showcase-text-page">
+                    <MDXProvider>
+                        <MDXRenderer>{text}</MDXRenderer>
+                    </MDXProvider>
+                </div>
+            </div>
         </section>
     )
 }
--- src/pages/werke-zur-deutschen-literatur.js
+++ src/pages/werke-zur-deutschen-literatur.js
@@ -1,57 +1,23 @@
 import React from 'react'
-
 import { graphql } from 'gatsby'
-import { MDXProvider } from '@mdx-js/react'
-import MDXRenderer from 'gatsby-plugin-mdx/mdx-renderer'
+import { getImage } from 'gatsby-plugin-image'
 
 import Layout from '../components/Layout'
 import Template from '../components/showCaseBox/ShowCaseBoxPage'
 
-import image from '../assets/images/Werke zur deutschen Literatur.jpg'
-
-const alt = 'Werke zur deutschen Literatur'
-/* const text = (
-    <p>
-        In den Schaffensjahren von Leo Leonhard war die Wechselbeziehung
-        zwischen Wort und Bild, zwischen Literatur und bildender Kunst von
-        größerer Bedeutung als heute. Viele Künstler waren Grenzgänger zwischen
-        Bildender Kunst und Literatur. Und so auch Leo Leonhard: Seine
-        künstlerische Praxis war vielfach durch die Literatur geprägt. Das Werk
-        Georg Büchners stellte einen Schwerpunkt seiner Arbeit dar – er hat sich
-        aber auch mit anderen Autoren wie Franz Kafka, Robert Walser, Edgar
-        Allen Poe, Ezra Pound, Christian Dietrich Grabbe, Christoph Martin
-        Wieland, Georg Christoph Lichtenberg oder Johann Wolfgang von Goethe
-        beschäftigt und deren Werke illustriert. Diese Nähe zu der Literatur
-        spiegelt sich aber auch in seiner Ölmalerei. Und es war Hugo von
-        Hofmannsthal, den er immer wieder gerne zitiert hat: „Die Tiefe muss man
-        verstecken. Wo? An der Oberfläche.“
-    </p>
-)
- */
 export default function Werke({ data }) {
-    // console.log(data)
     const myNodes = data.allContentfulShowcaseBox.nodes
     return (
         <Layout>
             {myNodes.map((item) => {
-                // console.log(item.text)
+                const image = getImage(item.image)
                 return (
-                    <section className="standard-layout-width">
-                        <div className="showcase-page">
-                            <img
-                                src={image}
-                                alt={alt}
-                                className="img-showcase"
-                            />
-                            <div className="showcase-text-page">
-                                <MDXProvider>
-                                    <MDXRenderer>
-                                        {item.text.childMdx.body}
-                                    </MDXRenderer>
-                                </MDXProvider>
-                            </div>
-                        </div>
-                    </section>
+                    <Template
+                        key={item.slug}
+                        alt={item.header}
+                        src={image}
+                        text={item.text.childMdx.body}
+                    />
                 )
             })}
         </Layout>
@@ -72,7 +38,7 @@ export const query = graphql`
                     }
                 }
                 image {
-                    gatsbyImageData(width: 525, placeholder: BLURRED)
+                    gatsbyImageData(width: 1200, placeholder: BLURRED)
                 }
             }
         }
