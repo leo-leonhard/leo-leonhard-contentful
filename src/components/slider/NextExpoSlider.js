716095525f8a1e17bd59edac22697f7e5dcfb8f5
darayaq-01 <daniel@darayaq.de>
2023-05-26 13:06:48 +0200

    update banner next exhibitions

--- src/components/slider/NextExpoSlider.js
+++ src/components/slider/NextExpoSlider.js
@@ -3,6 +3,62 @@ import { useStaticQuery, graphql, Link } from 'gatsby'
 import { getImage } from 'gatsby-plugin-image'
 import { BgImage } from 'gbimage-bridge'
 
+// The picture data is an array and always should have more than one picture in Model.
+// to get the picture already active you need to change the filter by order.
+// would also possible delete other pictures, if there are active more than one picture
+
+import './nextExpoSlider.css'
+
+export default function NextExpoSlider() {
+    const data = useStaticQuery(graphql`
+        query NextExpoQuery {
+            allContentfulNextExposition(filter: { nextorder: { eq: 1 } }) {
+                nodes {
+                    nexttitle
+                    link
+                    nextorder
+                    nextimage {
+                        id
+                        gatsbyImageData(
+                            layout: FULL_WIDTH
+                            placeholder: BLURRED
+                            aspectRatio: 1.5
+                        )
+                        filename
+                        url
+                    }
+                }
+            }
+        }
+    `)
+
+    return (
+        <div>
+            {data.allContentfulNextExposition.nodes.map((element) => {
+                const nextExpoImage = getImage(element.nextimage)
+                let link = element.link
+                return (
+                    <div key={element.nextorder}>
+                        <Link to={link}>
+                            <BgImage
+                                alt={element.nexttitle}
+                                image={nextExpoImage}
+                                className="bgimage"
+                            ></BgImage>
+                        </Link>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+/* 
+import React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+import { getImage } from 'gatsby-plugin-image'
+import { BgImage } from 'gbimage-bridge'
+
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
@@ -45,7 +101,7 @@ export default function NextExpoSlider() {
 
     return (
         <div>
-            <Slider {...settings}>
+            <Slider {...settings}> 
                 {data.allContentfulNextExposition.nodes.map((element) => {
                     const nextExpoImage = getImage(element.nextimage)
                     let link = element.link
@@ -66,3 +122,5 @@ export default function NextExpoSlider() {
         </div>
     )
 }
+
+*/
